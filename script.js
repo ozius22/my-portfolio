f284717975630bee3454772f2765d5c307f9a3ba
Luis <lsuizo72@gmail.com>
2025-03-02 20:02:43 +0800

    add a loading screen

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const loader = document.querySelector('#loader-container');
+const greeting = document.querySelector('#greeting');
+const header = document.querySelector('#header');
+const pangalan = document.querySelector('#pangalan');
+const cta = document.querySelector('#cta');
+
+setTimeout(() => {
+    loader.remove();
+    header.classList.remove('invisible')
+}, 3600); 
+
+setTimeout(() => {
+    greeting.classList.add('fade-in')
+}, 4000)
+
+setTimeout(() => {
+    pangalan.classList.add('fade-in')
+}, 5000)
+
+setTimeout(() => {
+    cta.classList.add('fade-in')
+}, 6500)
